da5a92397c95187655d1496daca4dca5fa1ea88c
Mohit <mohitkumawat12345613@gmail.com>
2023-04-19 20:59:52 +0200

    Change Color on a Click

---
+++ index.js
@@ -0,0 +1,37 @@
+// let button = document.getElementById("button");
+//     let randomcolor = () => {
+//         let index = '0123456789abcdef';
+//         let colour = "#";
+//         for (let i = 0; i<6; i++){ 
+//             colour = colour + index[Math.floor(Math.random()*16)];
+//         }    
+//         return  colour;
+//     }
+//     console.log(randomcolor);
+//     function changeColor(){
+//         document.body.style.backgroundColor = randomcolor();
+//     }
+//     button.addEventListener("click",changeColor);
+// const array = [1,2,3,4,5,6,7];ub
+// const sum = array.reduce((accumulator,current)=>
+//     accumulator - current,0);
+// console.log(sum);
+Sum();
+Sub();
+console.log(a);
+var a = 20;
+function Sum(){
+    console.log(x);
+    var x = 10;
+
+    var y = 10;
+    console.log(x + y);
+}
+function Sub(){
+    var x = 10;
+    var y = 10;
+    console.log(x - y);
+}
+
+
+
